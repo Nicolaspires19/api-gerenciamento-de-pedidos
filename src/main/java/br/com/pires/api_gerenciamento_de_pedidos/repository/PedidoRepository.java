4c2feac9c6dde08420d564ec594d32a2ee3843f6
nico1 <nico19pires@gmail.com>
2026-04-27 20:59:46 -0300

    UserService,UserDetails,UserDetailsService

--- src/main/java/br/com/pires/api_gerenciamento_de_pedidos/repository/PedidoRepository.java
+++ src/main/java/br/com/pires/api_gerenciamento_de_pedidos/repository/PedidoRepository.java
@@ -3,5 +3,4 @@ package br.com.pires.api_gerenciamento_de_pedidos.repository;
 import br.com.pires.api_gerenciamento_de_pedidos.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> { }
